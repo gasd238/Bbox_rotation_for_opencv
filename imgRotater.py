f4ba2ddd0f67f4fda599d33cf66a70676b819074
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-11-13 10:58:51 +0900

    Add files via upload

--- imgRotater.py
+++ imgRotater.py
@@ -7,7 +7,7 @@ import os
 from math import sin, cos
 import numpy as np
 
-angle = 225
+angle = 190
 
 class ImageRotater:
     def __init__(self):
@@ -18,13 +18,48 @@ class ImageRotater:
         self.txtpath = "./before"
         self.rotatedimgpath = "./after"
         self.rangle = angle * np.pi / 180
-            
         self.rot_matrix = np.array([[np.cos(self.rangle), -np.sin(self.rangle)], [np.sin(self.rangle), np.cos(self.rangle)]])
 
-    def rotatebbox(self, tmp):
-        for i in range(len(tmp)):
-            tmp[i] = int(tmp[i])
+    def get_img_txt(self):
+        self.imageList= glob.glob(os.path.join(self.imagepath, '*.JPG'))
+        self.txtList= glob.glob(os.path.join(self.txtpath, '*.txt'))
 
+    def make_folder(self):
+        if not os.path.isdir(self.rotatedimgpath):
+            os.mkdir(self.rotatedimgpath)
+        self.savefolder= os.path.join(self.rotatedimgpath, str(angle))  
+        if not os.path.isdir(self.savefolder):
+            os.mkdir(self.savefolder)
+
+    def rotateImage(self, img):
+        self.imagename = os.path.split(img)[-1].split('.')[0]
+        image = cv.imread(os.path.join(img))
+        rotated = imutils.rotate_bound(image, -angle)
+        self.h, self.w, self.c = image.shape
+        self.rh, self.rw, self.rc = rotated.shape
+        imagepath = os.path.join(self.savefolder, self.imagename+".jpg")
+        cv.imwrite(imagepath, rotated)
+
+    def update_txt(self, txt):
+        f = open(txt, "r")
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            tmp = line.strip("\n").split(" ")
+            if len(tmp) > 3:
+                tmp = self.rotatebbox(tmp)
+                # if angle == 90:
+                #     tmp = self.rotate90(tmp)
+                # elif angle == 180:
+                #     tmp = self.rotate180(tmp)
+            lines[lines.index(line)] = ' '.join(tmp)
+        lines = '\n'.join(lines)
+        f = open(os.path.join(self.savefolder, self.imagename+".txt"), "w")
+        f.write(lines)
+        f.close
+
+    def rotatebbox(self, tmp):
+        tmp = [int(item) for item in tmp]
         new_bbox = []
 
         if len(tmp) > 1:
@@ -57,19 +92,11 @@ class ImageRotater:
                     new_upper_left_corner.append(x_prime)
                     new_upper_left_corner.append(y_prime)
 
-            tmp = [new_upper_left_corner[0], new_upper_left_corner[1], new_lower_right_corner[0], new_lower_right_corner[1]]
+            new_bbox = [new_upper_left_corner[0], new_upper_left_corner[1], new_lower_right_corner[0], new_lower_right_corner[1]]
 
-            for i in range(len(tmp)):
-                tmp[i] = str(round(tmp[i]))
+            tmp = [str(round(item)) for item in new_bbox]
 
         return tmp
-
-    def make_folder(self):
-        if not os.path.isdir(self.rotatedimgpath):
-            os.mkdir(self.rotatedimgpath)
-        self.savefolder= os.path.join(self.rotatedimgpath, str(angle))  
-        if not os.path.isdir(self.savefolder):
-            os.mkdir(self.savefolder)
 
     # def rotate90(self, tmp):
     #     tmp[0], tmp[1]= tmp[1], tmp[0]
@@ -87,42 +114,13 @@ class ImageRotater:
     #     tmp[0], tmp[1], tmp[2], tmp[3]= tmp[2], tmp[3], tmp[0], tmp[1]
     #     return tmp
 
-    def get_img_txt(self):
-        self.imageList= glob.glob(os.path.join(self.imagepath, '*.JPG'))
-        self.txtList= glob.glob(os.path.join(self.txtpath, '*.txt'))
-
-    def imageRotate(self):
+    def Rotate(self):
         self.get_img_txt()
         self.make_folder()
         for img, txt in zip(self.imageList, self.txtList):
-            self.imagename = os.path.split(img)[-1].split('.')[0]
-            image = cv.imread(os.path.join(img))
-            rotated = imutils.rotate_bound(image, -angle)
-            self.h, self.w, self.c = image.shape
-            self.rh, self.rw, self.rc = rotated.shape
-            imagepath = os.path.join(self.savefolder, self.imagename+".jpg")
-            cv.imwrite(imagepath, rotated)
+            self.rotateImage(img)
             self.update_txt(txt)
-
-    def update_txt(self, txt):
-        f = open(txt, "r")
-        lines = f.readlines()
-        f.close()
-        for line in lines:
-            tmp = line.strip("\n").split(" ")
-            if len(tmp) > 3:
-                tmp = self.rotatebbox(tmp)
-                # if angle == 90:
-                #     tmp = self.rotate90(tmp)
-                # elif angle == 180:
-                #     tmp = self.rotate180(tmp)
-            lines[lines.index(line)] = ' '.join(tmp)
-        lines = '\n'.join(lines)
-        f = open(os.path.join(self.savefolder, self.imagename+".txt"), "w")
-        f.write(lines)
-        f.close
-
 
 if __name__ == "__main__":
     img = ImageRotater()
-    img.imageRotate()
+    img.Rotate()
